040cdde07a969e43112e44c3da61964289517cfc
Michel Araujo <micaraujo71@gmail.com>
2024-02-28 12:34:30 -0300

    🎉 Add initial project files and configurations

---
+++ desenvolvimento-de-software-II/detran-upf/src/app.ts
@@ -0,0 +1,31 @@
+import { ZodError } from 'zod'
+import cors from '@fastify/cors'
+import fastify from 'fastify'
+
+export const app = fastify()
+
+app.register(cors, {
+    origin: true,
+    credentials: true,
+})
+
+app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+        return reply.status(400).send({
+            message: 'Validation failed',
+            issues: error.format(),
+        })
+    }
+
+    if (error.statusCode) {
+        return reply.status(error.statusCode).send({
+            message: error.message,
+        })
+    }
+
+    console.error(error)
+
+    return reply.status(500).send({
+        message: 'Internal server error',
+    })
+})
