df50c593aac8638ee2ad762524c7176c39b03052
Michel Araujo <micaraujo71@gmail.com>
2024-03-08 19:39:26 -0300

    Add user registration use case and test

---
+++ desenvolvimento-de-software-II/detran-upf/api/__tests__/src/use-cases/users/register.spec.ts
@@ -0,0 +1,52 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryUsersRepository } from '../../../../src/repositories/in-memory/in-memory-users-repository'
+import { RegisterUseCase } from '../../../../src/use-cases/users/register'
+
+let usersRepository: InMemoryUsersRepository
+let sut: RegisterUseCase
+
+describe('Register User Use Case', () => {
+    beforeEach(() => {
+        usersRepository = new InMemoryUsersRepository()
+        sut = new RegisterUseCase(usersRepository)
+    })
+
+    it('should be able to register a new user', async () => {
+        const { user } = await sut.execute({
+            cpf: '12345678900',
+            dataNascimento: new Date(),
+            descricaoEndereco: 'Rua teste',
+            email: 'johndoe@example.com',
+            nomePessoa: 'John Doe',
+            pcd: false,
+            telefone: 1234567890,
+        })
+
+        expect(user.cpf).toEqual(expect.any(String))
+    })
+
+    it('should not be able to register a user with an existing CPF', async () => {
+        await sut.execute({
+            cpf: '12345678900',
+            dataNascimento: new Date(),
+            descricaoEndereco: 'Rua teste',
+            email: 'johndoe@example.com',
+            nomePessoa: 'John Doe',
+            pcd: false,
+            telefone: 1234567890,
+        })
+
+        await expect(
+            sut.execute({
+                cpf: '12345678900',
+                dataNascimento: new Date(),
+                descricaoEndereco: 'Rua teste',
+                email: 'johndoe@example.com',
+                nomePessoa: 'John Doe',
+                pcd: false,
+                telefone: 1234567890,
+            }),
+        ).rejects.toThrow('User already exists.')
+    })
+})
---
+++ desenvolvimento-de-software-II/detran-upf/api/src/use-cases/users/register.ts
@@ -0,0 +1,52 @@
+import { User } from '@prisma/client'
+import { UsersRepository } from '../../repositories/users-repository'
+
+interface RegisterUseCaseRequestDTO {
+    nomePessoa: string
+    cpf: string
+    descricaoEndereco: string
+    dataNascimento: Date
+    telefone: number
+    email: string
+    pcd: boolean
+}
+
+interface RegisterUseCaseResponseDTO {
+    user: User
+}
+
+export class RegisterUseCase {
+    constructor(private usersRepository: UsersRepository) {
+        Object.freeze(this.usersRepository)
+    }
+
+    async execute({
+        nomePessoa,
+        cpf,
+        dataNascimento,
+        descricaoEndereco,
+        telefone,
+        email,
+        pcd,
+    }: RegisterUseCaseRequestDTO): Promise<RegisterUseCaseResponseDTO> {
+        const userAlreadyExists = await this.usersRepository.findUserByCpf(cpf)
+
+        if (userAlreadyExists) {
+            throw new Error('User already exists.')
+        }
+
+        const user = await this.usersRepository.create({
+            nome_pessoa: nomePessoa,
+            cpf,
+            data_nascimento: dataNascimento,
+            pcd,
+            descricao_endereco: descricaoEndereco,
+            email,
+            telefone,
+        })
+
+        return {
+            user,
+        }
+    }
+}
