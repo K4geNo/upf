cee83b5c86248ab6273a45128d7b6065b5acd776
Michel <micaraujo71@gmail.com>
2024-03-19 21:26:08 -0300

    Adiciona método findAll() ao repositório de usuários

--- desenvolvimento-de-software-II/detran-upf/api/src/repositories/users-repository.ts
+++ desenvolvimento-de-software-II/detran-upf/api/src/repositories/users-repository.ts
@@ -3,6 +3,7 @@ import { Prisma, User } from '@prisma/client'
 export interface UsersRepository {
     findUserById(id: string): Promise<User | null>
     findUserByCpf(cpf: string): Promise<User | null>
+    findAll(): Promise<User[]>
     create(data: Prisma.UserCreateInput): Promise<User>
     update(data: Prisma.UserUncheckedUpdateInput, userId: string): Promise<User>
 }
