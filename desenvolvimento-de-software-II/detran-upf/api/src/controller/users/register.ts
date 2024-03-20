a47b185659745656ad5c87d521b008832b90702c
Michel <micaraujo71@gmail.com>
2024-03-19 21:26:32 -0300

    Adiciona controlador de registro de usuários

---
+++ desenvolvimento-de-software-II/detran-upf/api/src/controller/users/register.ts
@@ -0,0 +1,35 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '../../use-cases/users/register'
+import { registerUserSchema } from '../../schema/users/register-schema'
+
+export async function registerController(
+    request: FastifyRequest,
+    reply: FastifyReply,
+) {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const userRegisterUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    const {
+        cpf,
+        dataNascimento,
+        descricaoEndereco,
+        email,
+        nomePessoa,
+        pcd,
+        telefone,
+    } = registerUserSchema.parse(request.body)
+
+    const user = await userRegisterUseCase.execute({
+        cpf,
+        dataNascimento,
+        descricaoEndereco,
+        email,
+        nomePessoa,
+        pcd,
+        telefone,
+    })
+
+    return reply.code(201).send(user)
+}
