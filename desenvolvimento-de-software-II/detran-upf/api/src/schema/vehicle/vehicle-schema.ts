e888ffc8d0db4b71192b78659202ca79e71ca8cd
KageNo <micaraujo71@gmail.com>
2024-04-08 17:14:13 -0300

    🍱 Atualiza esquemas de veículo para adicionar parâmetros de busca por placa e atualização parcial

--- desenvolvimento-de-software-II/detran-upf/api/src/schema/vehicle/vehicle-schema.ts
+++ desenvolvimento-de-software-II/detran-upf/api/src/schema/vehicle/vehicle-schema.ts
@@ -10,3 +10,23 @@ export const registerVehicleSchema = z.object({
     licensePlate: z.string(),
     type: z.number(),
 })
+
+export const findByLicensePlateParamsSchema = z.object({
+    licensePlate: z.string(),
+})
+
+export const findVehicleByVehicleIdParamsSchema = z.object({
+    vehicleId: z.string(),
+})
+
+export const updateVehicleSchema = z
+    .object({
+        manufacturingYear: z.number(),
+        color: z.string(),
+        ipvaPaid: z.boolean(),
+        ipvaValue: z.number(),
+        brand: z.string(),
+        model: z.string(),
+        type: z.number(),
+    })
+    .partial()
